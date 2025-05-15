f16cac1372b4f111c973cb7f559378432d889685
David-Mdrs <medeiros.david.ac@gmail.com>
2025-05-15 20:52:06 -0300

    Motores

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -68,6 +68,10 @@ void DisplaySeteSegHexa();
 void DisplaySeteSegHexa2D();
 void Genius();
 void SensorUltrassonico();
+void MotorDC();
+void MicroServomotor();
+
+int contador = 0;
 
 
 /* USER CODE END PFP */
@@ -115,11 +119,14 @@ int main(void)
 //	LCD_Init(4, 20);
 
 
-
-//	GPIO_Pin_Mode(GPIOE, PIN_0, INPUT);
-//	GPIO_Resistor_Enable(GPIOE, PIN_0, PULL_UP);
-//	GPIO_Pin_Mode(GPIOE, PIN_4, OUTPUT);
-
+	// MOTOR
+	GPIO_Clock_Enable(GPIOE);
+	GPIO_Pin_Mode(GPIOE, PIN_0, OUTPUT);		// A1
+	GPIO_Pin_Mode(GPIOE, PIN_1, OUTPUT);	// A2
+	GPIO_Pin_Mode(GPIOE, PIN_2, OUTPUT);	// B1
+	GPIO_Pin_Mode(GPIOE, PIN_3, OUTPUT);	// B2
+	GPIO_Pin_Mode(GPIOE, PIN_4, OUTPUT);	// PWM1
+	GPIO_Pin_Mode(GPIOE, PIN_5, OUTPUT);	// PWM2
 
 
   /* USER CODE END 2 */
@@ -128,29 +135,81 @@ int main(void)
   /* USER CODE BEGIN WHILE */
 
 
-	// MOTOR
-	//	GPIO_Clock_Enable(GPIOE);
-	//	GPIO_Pin_Mode(GPIOE, PIN_0, OUTPUT);
-	//	GPIO_Pin_Mode(GPIOE, PIN_1, OUTPUT);
-	//	GPIO_Pin_Mode(GPIOE, PIN_2, OUTPUT);
-
-
-
 	while (1) {
-
-		SensorUltrassonico();
+		// MicroServomotor();
 
 
 		// MOTOR
-//		GPIO_Write_Pin(GPIOE, PIN_0, LOW);
-//		GPIO_Write_Pin(GPIOE, PIN_1, HIGH);
-//		AtivarModoPWM(GPIOE, GPIO_PIN_2, 2);
-//		Delay_ms(1000);
-//
-//		GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
-//		GPIO_Write_Pin(GPIOE, PIN_1, LOW);
-//		AtivarModoPWM(GPIOE, GPIO_PIN_2, 2);
-//		Delay_ms(1000);
+		for(int i = 0; i < 100; i++) {
+			GPIO_Write_Pin(GPIOE, PIN_4, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_5, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_2, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_0, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_3, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_1, HIGH);
+			Delay_ms(10);
+
+			GPIO_Write_Pin(GPIOE, PIN_4, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_5, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_2, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_3, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_1, HIGH);
+			Delay_ms(10);
+
+			GPIO_Write_Pin(GPIOE, PIN_4, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_5, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_2, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_3, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_1, LOW);
+			Delay_ms(10);
+
+			GPIO_Write_Pin(GPIOE, PIN_4, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_5, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_2, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_0, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_3, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_1, LOW);
+			Delay_ms(10);
+		}
+
+		for(int i = 0; i < 50; i++) {
+			GPIO_Write_Pin(GPIOE, PIN_4, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_5, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_2, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_0, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_3, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_1, LOW);
+			Delay_ms(10);
+
+			GPIO_Write_Pin(GPIOE, PIN_4, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_5, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_2, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_3, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_1, LOW);
+			Delay_ms(10);
+
+			GPIO_Write_Pin(GPIOE, PIN_4, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_5, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_2, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_3, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_1, HIGH);
+			Delay_ms(10);
+
+			GPIO_Write_Pin(GPIOE, PIN_4, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_5, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_2, HIGH);
+			GPIO_Write_Pin(GPIOE, PIN_0, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_3, LOW);
+			GPIO_Write_Pin(GPIOE, PIN_1, HIGH);
+			Delay_ms(10);
+		}
+
+		// SensorUltrassonico();
+		// MotorDC();
 
 
 
@@ -386,17 +445,15 @@ void Despertador(GPIO_TypeDef* porta, uint16_t pino) {
 void AtivarModoPWM(GPIO_TypeDef* porta, uint16_t pino, int velocidade) {
 	for(int i = 0; i < 2000; i += velocidade) {
 		HAL_GPIO_WritePin(porta, pino, GPIO_PIN_RESET);
-		Delay_us(i);
-		HAL_GPIO_WritePin(porta, pino, GPIO_PIN_SET);
 		Delay_us(1999 - i);
+		HAL_GPIO_WritePin(porta, pino, GPIO_PIN_SET);
+		Delay_us(i);
 	}
-	Delay_ms(1000);
-
 	for(int i = 0; i < 2000; i += velocidade) {
 		HAL_GPIO_WritePin(porta, pino, GPIO_PIN_RESET);
-		Delay_us(1999 - i);
-		HAL_GPIO_WritePin(porta, pino, GPIO_PIN_SET);
 		Delay_us(i);
+		HAL_GPIO_WritePin(porta, pino, GPIO_PIN_SET);
+		Delay_us(1999 - i);
 	}
 }
 
@@ -669,14 +726,7 @@ void Genius() {
 				GPIO_Write_Pin(GPIOE, PIN_4, HIGH);
 				Delay_ms(300);
 				GPIO_Write_Pin(GPIOE, PIN_4, LOW);
-				if(sequencia[entrada] != PIN_4) {
-					for(int i = 0; i < 6; i++) {
-						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_4);
-						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_5);
-						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_6);
-						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_7);
-						Delay_ms(500);
-					}
+
 					entrada = tamanho = 0;
 				} else {
 					entrada++;
@@ -801,44 +851,61 @@ void SensorUltrassonico() {
 	}
 }
 
-void SensorUltrassonico2() {
+void MotorDC() {
 	GPIO_Clock_Enable(GPIOE);
-	GPIO_Pin_Mode(GPIOE, PIN_0, INPUT);		// ECHO
-	GPIO_Pin_Mode(GPIOE, PIN_1, OUTPUT);	// TRIG
-	GPIO_Write_Pin(GPIOE, PIN_1, LOW);
-	GPIO_Pin_Mode(GPIOE, PIN_2, OUTPUT);	// BUZZER
+	GPIO_Pin_Mode(GPIOE, PIN_0, OUTPUT);
+	GPIO_Pin_Mode(GPIOE, PIN_1, OUTPUT);
+	GPIO_Pin_Mode(GPIOE, PIN_2, OUTPUT);
 
-	unsigned int distancia = 0;
-	int tempo = 0;
+	Delay_ms(1000);
 
 	while (1) {
-
-		// Enviando pulso
-		Delay_ms(10);
+		// Mudando sentido e ativando PWM
+		GPIO_Write_Pin(GPIOE, PIN_0, LOW);
 		GPIO_Write_Pin(GPIOE, PIN_1, HIGH);
-		Delay_us(10);
-		GPIO_Write_Pin(GPIOE, PIN_1, LOW);
+		AtivarModoPWM(GPIOE, GPIO_PIN_2, 2);
 
-		// Iniciando contagem
-		while(!GPIO_Read_Pin(GPIOE, PIN_0));
-		tempo = 0;
-		while(GPIO_Read_Pin(GPIOE, PIN_0)) {
-			Delay_us(1);
-			tempo++;
+		// Mudando sentido e ativando PWM
+		GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
+		GPIO_Write_Pin(GPIOE, PIN_1, LOW);
+		AtivarModoPWM(GPIOE, GPIO_PIN_2, 2);
+	}
+}
+
+void MicroServomotor() {
+	GPIO_Clock_Enable(GPIOE);
+	GPIO_Pin_Mode(GPIOE, PIN_0, OUTPUT);
+	GPIO_Pin_Mode(GPIOE, PIN_1, OUTPUT);
+	GPIO_Pin_Mode(GPIOE, PIN_2, INPUT);
+	GPIO_Pin_Mode(GPIOE, PIN_3, INPUT);
+	GPIO_Resistor_Enable(GPIOE, PIN_2, PULL_UP);
+	GPIO_Resistor_Enable(GPIOE, PIN_3, PULL_UP);
+
+	contador = 500;
+	while (1) {
+
+		if(!GPIO_Read_Pin(GPIOE, PIN_3)) {	// Botão esquerdo
+			GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
+			Delay_us(contador);
+			GPIO_Write_Pin(GPIOE, PIN_0, LOW);
+			Delay_us(20000 - contador);
+			if(contador < 2500) {
+				contador += 10;
+			}
 		}
 
-		// Calculando distância
-		distancia = tempo/58;
-
-		// Acionando LED
-		if(distancia > 50) {
-			GPIO_Write_Pin(GPIOE, PIN_2, LOW);
-		} else {
-			GPIO_Write_Pin(GPIOE, PIN_2, HIGH);
-			Delay_ms(distancia * 10);
+		if(!GPIO_Read_Pin(GPIOE, PIN_2)) {	// Botão esquerdo
+			GPIO_Write_Pin(GPIOE, PIN_0, HIGH);
+			Delay_us(contador);
+			GPIO_Write_Pin(GPIOE, PIN_0, LOW);
+			Delay_us(20000 - contador);
+			if(contador > 500) {
+				contador -= 10;
+			}
 		}
 	}
 }
+
 
 /* USER CODE END 4 */
 
